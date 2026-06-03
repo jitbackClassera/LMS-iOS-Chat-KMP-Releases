a594ad85a359c6eb4f91c65429cfe0858aad2399
osamanathmi <osamanathmi@gmail.com>
2026-06-04 00:24:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Chat-KMP-Sdk",
+    name: "ChatKMPSdk",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "Chat-KMP-Sdk", targets: ["Chat-KMP-Sdk"])
+        .library(name: "ChatKMPSdk", targets: ["ChatKMPSdk"])
     ],
     targets: [
         .binaryTarget(
-            name: "Chat-KMP-Sdk",
+            name: "ChatKMPSdk",
             url: "https://github.com/jitbackClassera/LMS-iOS-Chat-KMP-Releases/releases/download/1.0.0/ChatSdk.xcframework.zip",
             checksum: "74ac00dc97fddcdbe0d7bdc2b198e45a2c6e251fc6751cc2dc515b5b36162704"
         )
