0db13e8d411117d15eba6e1306dc81dce3502867
jitbackClassera <Jitpack@classera.com>
2026-08-11 15:30:15 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatLms",
-            url: "https://github.com/jitbackClassera/LMS-iOS-Chat-KMP-Releases/releases/download/1.0.7/ChatLms.xcframework.zip",
-            checksum: "875cc7386f8bc68cda4754fce920e8c60cde2a23f9798f858478accc7c7c17fb"
+            url: "https://github.com/jitbackClassera/LMS-iOS-Chat-KMP-Releases/releases/download/1.0.8/ChatLms.xcframework.zip",
+            checksum: "1f4d35afef9e620461d524c3fa9cd60e77be6f94c69f2b585299b09358bde620"
         )
     ]
 )
